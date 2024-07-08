ef342b099ad4e4a7b0312e1f12dd6aad8204d8f1
ninafinageeva <finageeva.nina@mail.ru>
2024-07-09 00:06:36 +0300

    d.z.25.2.4 tests and coverage

--- materials/paginators.py
+++ materials/paginators.py
@@ -1,14 +1,14 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import pagination
 
 
-class CoursePaginator(PageNumberPagination):
+class CoursePaginator(pagination.PageNumberPagination):
     """ Пагинация для вывода курсов. """
     page_size = 10
     page_query_param = 'page_size'
     max_page_size = 100
 
 
-class LessonPaginator(PageNumberPagination):
+class LessonPaginator(pagination.PageNumberPagination):
     """ Пагинация для вывода уроков. """
     page_size = 10
     page_query_param = 'page_size'
